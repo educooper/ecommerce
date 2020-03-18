6959ef205e4f5d099d4a25841223454d64d2576e
Edu Cooper <edusardo@uninove.edu.br>
2020-03-18 05:52:43 -0300

    admin implementado

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
@@ -17,6 +18,14 @@ $app->get('/', function() {
 //	$sql = new Hcode\DB\Sql();
 //	$results = $sql->select("SELECT * FROM tb_users");
 //	echo json_encode($results);
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+
 });
 
 $app->run();
