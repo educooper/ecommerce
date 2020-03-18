b1e7aa01956b4d5ba80af3033f5bff9b01ccb9bf
Edu Cooper <edusardo@uninove.edu.br>
2020-03-18 06:41:59 -0300

    Model/model em construcao

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new \Slim\Slim();
 
@@ -25,6 +26,26 @@ $app->get('/admin', function() {
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
 
 });
 
