6f944f8acba29d992fb4c6282148cb6b7e836d15
Edu Cooper <edusardo@uninove.edu.br>
2020-03-17 21:46:53 -0300

    subindo todo o sistema

--- index.php
+++ index.php
@@ -2,18 +2,21 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	$page = new Page();
 
-	echo json_encode($results);
-
+	$page->setTpl("index");
+//	$sql = new Hcode\DB\Sql();
+//	$results = $sql->select("SELECT * FROM tb_users");
+//	echo json_encode($results);
 });
 
 $app->run();
---
+++ views-cache/header.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -0,0 +1,7 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hcode Store</title>
+
+</head>
+    <body>
